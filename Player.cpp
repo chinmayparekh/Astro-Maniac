cc5cbe3289d8d7d3b2cc619da613bfdaedaa4905
chinmayparekh <chinmayparekh11@gmail.com>
2021-12-18 01:44:43 +0530

    fixed conflict

--- Player.cpp
+++ Player.cpp
@@ -57,99 +57,9 @@ void Player::handleEvents()
 
 void Player::Render()
 {
-<<<<<<< HEAD
     //cout << move << endl;
     if ((vel_X > 0 || move < 30) && dir == 2)
     {
-=======
-    move++;
-    cout << move << endl;
-    if ((vel_X > 0 || move < 30) && dir == 2)
-    {
-        cout << move << endl;
-        moving = true;
-        if (move < 6)
-            objTexture = SDL_CreateTextureFromSurface(Game::renderer, IMG_Load("images/right0.png"));
-        else if (move < 12)
-            objTexture = SDL_CreateTextureFromSurface(Game::renderer, IMG_Load("images/right1.png"));
-        // else if(move < 45)
-        //     objTexture = SDL_CreateTextureFromSurface(Game::renderer, IMG_Load("images/right2.png"));
-        else if (move < 18)
-            objTexture = SDL_CreateTextureFromSurface(Game::renderer, IMG_Load("images/right2.png"));
-        else if (move < 24)
-            objTexture = SDL_CreateTextureFromSurface(Game::renderer, IMG_Load("images/right1.png"));
-        else
-            objTexture = SDL_CreateTextureFromSurface(Game::renderer, IMG_Load("images/right0.png"));
-    }
-    else if ((vel_X < 0 || move < 30) && dir == 1)
-    {
-        moving = true;
-        if (move < 6)
-            objTexture = SDL_CreateTextureFromSurface(Game::renderer, IMG_Load("images/left0.png"));
-        else if (move < 12)
-            objTexture = SDL_CreateTextureFromSurface(Game::renderer, IMG_Load("images/left1.png"));
-        // else if(move < 45)
-        //     objTexture = SDL_CreateTextureFromSurface(Game::renderer, IMG_Load("images/left2.png"));
-        else if (move < 18)
-            objTexture = SDL_CreateTextureFromSurface(Game::renderer, IMG_Load("images/left2.png"));
-        else if (move < 24)
-            objTexture = SDL_CreateTextureFromSurface(Game::renderer, IMG_Load("images/left1.png"));
-        else
-            objTexture = SDL_CreateTextureFromSurface(Game::renderer, IMG_Load("images/left0.png"));
-    }
-    else if (moving)
-    {
-        moving = false;
-        dir = 0;
-        objTexture = SDL_CreateTextureFromSurface(Game::renderer, IMG_Load("images/astronaut.png"));
-    }
-    Render();
-}
-
-void Player::handleEvents2()
-{
-    if (Game::event.type == SDL_KEYDOWN)
-    {
-        move = 0;
-        switch (Game::event.key.keysym.sym)
-        {
-        case SDLK_a: //going right
-            if (getX() > 745)
-            {
-                vel_X = 0;
-            }
-            else
-            {
-                vel_X = 5;
-                dir = 2;
-            }
-            break;
-        case SDLK_d:
-            if (getX() < 0)
-            {
-                vel_X = 0;
-            }
-            else
-            {
-                vel_X = -5;
-                dir = 1;
-            }
-            break;
-        }
-    }
-    else
-    {
-        vel_X = 0;
-    }
-    setX(getX() + vel_X);
-}
-
-void Player::Render2()
-{
-    //cout << move << endl;
-    if ((vel_X > 0 || move < 30) && dir == 2)
-    {
->>>>>>> 5336013eea9db7d5a2faf8fb1a8e0661e7a344ab
         move++;
         moving = true;
         int cf;
@@ -158,11 +68,6 @@ void Player::Render2()
         //     cf = 4 + (SDL_GetTicks() * 12 / 1000) % 4;
         // else
         //     cf = 8 - (SDL_GetTicks() * 12 / 1000) % 4;
-<<<<<<< HEAD
-
-=======
-        
->>>>>>> 5336013eea9db7d5a2faf8fb1a8e0661e7a344ab
         if (move < 3)
             cf = 8;
         else if (move < 6)
@@ -179,11 +84,7 @@ void Player::Render2()
         //     cf = 5;
         else
             cf = 4;
-<<<<<<< HEAD
         Animate(cf, dir);
-=======
-        Animate2(cf, dir);
->>>>>>> 5336013eea9db7d5a2faf8fb1a8e0661e7a344ab
     }
     else if ((vel_X < 0 || move > -30) && dir == 1)
     {
@@ -230,11 +131,7 @@ void Player::Render2()
         //     cf = 3;
         else
             cf = 4;
-<<<<<<< HEAD
         Animate(cf, dir);
-=======
-        Animate2(cf, dir);
->>>>>>> 5336013eea9db7d5a2faf8fb1a8e0661e7a344ab
     }
     else if (moving)
     {
